f4f4e6094f133902c6b77da8d3c29f53b4a260a2
Cooper Koers <133537769+cooperkoers@users.noreply.github.com>
2024-02-01 13:26:59 -0500

    updated exhuastive method to be universal and file-based rather than read-based ratio

--- src/vep_sim/exhaustive.py
+++ src/vep_sim/exhaustive.py
@@ -68,17 +68,9 @@ def exhaustive(path):
         missense_muts_per_codon[codon] = num_missense_muts
         nonsense_muts_per_codon[codon] = num_nonsense_muts
             
-
-    #load the cDNA fasta file from NCBI
-    #Downloaded the per-gene transcript 
-    #wget https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh37_latest/refseq_identifiers/GRCh37_latest_rna.fna.gz
-    
     fasta = pysam.FastaFile(path)
 
     data = {
-        'ensg':[],
-        'enst':[],
-        'gene_name':[],
         'has_ATG_start':[],
         'stop_codon_end':[],
         'cds_length':[],
@@ -91,19 +83,15 @@ def exhaustive(path):
     for ref in fasta.references:
         # fetching gene coding region data
         seq = fasta.fetch(ref)
-        ref_data = ref.split('|')
-        enst = ref_data[0]
-        ensg = ref_data[1]
-        gene_name = ref_data[5]
-        transcript_length = int(ref_data[6])
+        #print(seq + '\n' + '\n')  #debugging
         
         # string manipulation to isolate end and start of cds
-        cds = [s for s in ref_data if s.startswith("CDS:")]
-        start, end = cds[0].split(':')[1].split('-')
+        #cds = [s for s in ref_data if s.startswith("CDS:")]
+        #start, end = cds[0].split(':')[1].split('-')
         # type converstion to int
-        start = int(start)
-        end = int(end)
-        cds_seq = seq[start-1:end]
+        #start = int(start)
+        #end = int(end)
+        #cds_seq = seq[start-1:end]
         
         #sanity checks
         #- the cDNA seq length should divisible by 3
@@ -114,17 +102,17 @@ def exhaustive(path):
         #assert cds_seq[-3:] in {'TAA','TGA','TAG'} #same with this
         
         has_atg_start = False
-        if cds_seq[:3] == "ATG":
+        if seq[:3] == "ATG":
             has_atg_start = True
         
         stop_codons = ["TAA", "TGA", "TAG"]
         stop_codon_end = False
-        if cds_seq[-3:] in stop_codons:
+        if seq[-3:] in stop_codons:
             stop_codon_end = True
         
         
         #count how many of each type of codon there are
-        codon_frequency = collections.Counter(cds_seq[i:i+3] for i in range(0, len(cds_seq), 3))
+        codon_frequency = collections.Counter(seq[i:i+3] for i in range(0, len(seq), 3))
         
         #remove "codons" that are less than 3 basepairs (I don't know why the transcript lens aren't all div by 3)
         codon_frequency = {codon:count for codon,count in codon_frequency.items() if len(codon) == 3}
@@ -146,37 +134,35 @@ def exhaustive(path):
         )
 
         #update the "data" dictionary
-        data['ensg'].append(ensg)
-        data['enst'].append(enst)
-        data['gene_name'].append(gene_name)
+        #do not need to store ensg, enst, gene_name
         data['has_ATG_start'].append(has_atg_start)
         data['stop_codon_end'].append(stop_codon_end)
-        data['cds_length'].append(len(cds_seq))
         data['num_codons'].append(sum(codon_frequency.values()))
         data['num_synonymous'].append(num_synonymous)
         data['num_missense'].append(num_missense)
         data['num_nonsense'].append(num_nonsense)
-        
-        
+
+
     #close the fasta file
     fasta.close()
 
 
     #convert the "data" dictionary into dnds
+    #use the file-wide methodology
     dnds_method_1 = (sum(data['num_missense']) + sum(data['num_nonsense'])) / sum(data['num_synonymous'])
-    dnds_method_2 = []
-    for i in range(len(data['gene_name'])):
-        if data['num_synonymous'][i] == 0:
-            continue
-        dnds_method_2.append((data['num_missense'][i] + data['num_nonsense'][i]) / data['num_synonymous'][i])
+    #dnds_method_2 = []
+    #for i in range(len(data['gene_name'])):
+    #    if data['num_synonymous'][i] == 0:
+    #        continue
+    #    dnds_method_2.append((data['num_missense'][i] + data['num_nonsense'][i]) / data['num_synonymous'][i])
     
-    dnds2_mean = np.mean(dnds_method_2)
+    #dnds2_mean = np.mean(dnds_method_2)
 
     #convert the "data" dictionary to be a pandas table
     #df = pd.DataFrame(data)
     #df['dnds'] = (df['num_nonsense'] + df['num_missense'])/df['num_synonymous']
     #dnds_values = df.iloc[-1].tolist()
    
-    return(dnds_method_1, dnds2_mean)
+    return(dnds_method_1)
 
 
